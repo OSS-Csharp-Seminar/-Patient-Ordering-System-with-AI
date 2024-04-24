b35067589962502c201324ae7014a75e7a1aaa3c
josipdom03 <122830825+josipdom03@users.noreply.github.com>
2024-04-24 13:06:33 +0200

    Model upload
    Model upload

---
+++ Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace HospitalApp.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+}
---
+++ Models/Specialization.cs
@@ -0,0 +1,9 @@
+﻿namespace HospitalApp.Models
+{
+    public class Specialization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
